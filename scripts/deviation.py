e032bed4e27700390eef18e207f8634bde2c5a24
Akshay Chaturvedi <akshay.chaturvedi@vaahaq.com>
2022-06-02 09:47:43 +0530

    modify base image

--- scripts/deviation.py
+++ scripts/deviation.py
@@ -9,7 +9,7 @@ import random
 from functools import cmp_to_key
 # loading parameter jason file
 
-BASE_PATH = "/home/user/frinks/python_backend"
+BASE_PATH = "/home/poop/frinks/skh/python_backend"
 
 data_jsonx = json.load(open(f"{BASE_PATH}/scripts/deviation_data.json",))[0]
 
